a33e43b3ed67eb297c1b0c77a13d7790e194699c
Aaron Alfonseca <alfonzka_13@hotmail.com>
2022-11-23 14:18:08 -0600

    Se ven temas de las excepciones y se hace de practica una calculadora desde consola

---
+++ calculadora.py
@@ -0,0 +1,36 @@
+opcion=input("Que quieres hacer. 1.Sumar 2.Restar 3.Multiplicacion")
+
+continuar=True
+while continuar:
+    if opcion=="1":
+        pass
+    elif opcion=="2":
+        pass
+    elif opcion=="3":
+        pass
+    elif opcion=="4":
+        pass
+
+def suma():
+    num1=float(input("Ingresa un numero: "))
+    num2=float(input("Ingresa otro numero: "))
+    resultado=num1+num2
+    print(resultado)
+
+def resta():
+    num1=float(input("Ingresa un numero: "))
+    num2=float(input("Ingresa otro numero: "))
+    resultado=num1-num2
+    print(resultado)
+
+def multiplicacion():
+    num1=float(input("Ingresa un numero: "))
+    num2=float(input("Ingresa otro numero: "))
+    resultado=num1+num2
+    print(resultado)
+
+def division():
+    num1=float(input("Ingresa un numero: "))
+    num2=float(input("Ingresa otro numero: "))
+    resultado=num1/num2
+    print(resultado)
---
+++ return.py
@@ -0,0 +1,7 @@
+def suma(num1,num2):
+    result=num1+num2
+    return result
+
+operacion=suma(2,2)
+resta= 5 -operacion
+print(resta)
